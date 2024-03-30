9febbba55539919e4f0f3bb2a8a7196c0ee58c69
konung-yaropolk <yaropolk1995@gmail.com>
2024-03-30 02:56:08 -0500

    Add files via upload

--- ABF-AP-pattern-plot.py
+++ ABF-AP-pattern-plot.py
@@ -1,14 +1,15 @@
 import pyabf
 import matplotlib.pyplot as plt
 
-abf = pyabf.ABF("F:\\Lab Work Files\\Patch-clamp data\\TRP project\\2024_03_26_M2\\2024_03_26_0006.abf")
+abf = pyabf.ABF('F:\\Lab Work Files\\Patch-clamp data\\TRP project\\2024_03_26_M2\\2024_03_26_0006.abf')
 
 # use a custom colormap to create a different color for every sweep
-cm = plt.get_cmap("winter")
+cm = plt.get_cmap('winter')
 colors = [cm(x/abf.sweepCount) for x in abf.sweepList]
 # colors.reverse()
 
 plt.figure(figsize=(8, 5))
+plt.suptitle('AP firing pattern')
 for sweepNumber in abf.sweepList:
     abf.setSweep(sweepNumber)
     i1, i2 = 0, int(abf.sampleRate * 1)
